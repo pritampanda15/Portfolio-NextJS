3366430735760ac77bbaa3902abfab30f0d8a8fa
Pritam Kumar Panda <pritampkp15@gmail.com>
2024-12-09 15:56:22 +0100

    updated

--- src/pages/api/news.js
+++ src/pages/api/news.js
@@ -2,14 +2,14 @@ export default function handler(req, res) {
   const news = [
       {
           title: "Excited to Join Stanford as a Postdoc!",
-          excerpt: "Thrilled to announce that I'll be joining Stanford University as a Postdoctoral Scholar in the Department of Anesthesiology, Perioperative, and Pain Medicine at Stanford University School of Medicine. His current research is centered on designing novel anesthetics suitable for battlefield conditions. Leveraging AI-driven protein design and high-throughput virtual screening, he aims to accelerate the drug discovery process and develop effective anesthetic agents.",
+          excerpt: "Thrilled to announce that I'll be joining Stanford University as a Postdoctoral Scholar in the Department of Anesthesiology, Perioperative, and Pain Medicine at Stanford University School of Medicine. My current research is centered on designing novel anesthetics suitable for battlefield conditions. Leveraging AI-driven protein design and high-throughput virtual screening, he aims to accelerate the drug discovery process and develop effective anesthetic agents.",
           time: "Stanford University", // Add your preferred date and time format
           link: "https://profiles.stanford.edu/pritam-panda", // Update with your profile or announcement link
       },
       {
           title: "The ABCs of AlphaFold 3, Boltz, and Chai-1",
           excerpt: "I recently came across The ABCs of AlphaFold 3, Boltz, and Chai-1, a fascinating report exploring the advancements in AI-based protein structure prediction and modeling by Brian Naughton. How do you envision integrating these technologies into your workflows?",
-          time: "Boolean Biotech", // Add your preferred date and time format
+          time: "Boolean Biotech",// Add your preferred date and time format
           link: "https://www.linkedin.com/feed/update/urn:li:activity:7268935224925794304/",
       },
       {
